79aa8648ba8dc921f97a1a211b9206bbaad92235
Matthew Saber <mattsaber21@yahoo.com>
2021-07-29 12:50:31 -0700

    API KEY in env and secrets, lots of article events and some state

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -16,3 +16,36 @@ ArticleList.propTypes = {
 
 export default ArticleList;
 
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+const CharacterList = ({ characters }) => {
+  const characterElements = characters.map((character) => (
+    <li key={character.id}>
+      <Character
+        {...character}
+        // name={character.name}
+        // species={character.species}
+        // status={character.status}
+        // image={character.image}
+      />
+    </li>
+  ));
+
+  return <ul>{characterElements}</ul>;
+};
+
+CharacterList.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      species: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default CharacterList;
--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,15 +1,40 @@
 /* eslint-disable max-len */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 //import ArticleList, Search, API call from services
+import ArticleList from '../components/articles/ArticleList';
+import Search from '../components/search/Search';
+import { getArticles } from '../services/news-utils';
 //setState w/articles and search, componentDidUpdate for API call
-//handleChange for props, onSubmit from Search (make sure the props are stated in Search also)
-//l
 export default class NewsSearch extends Component {
+  state = {
+    search: '',
+    articles: [],
+    // loading: true,
+  };
+  //handleChange for props, onSubmit from Search (make sure the props are stated in Search also)
+  handleChange = ({ target }) => {
+    this.setState({ search: target.value });
+  };
+  //double check this, fingers crossed
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const articles = await getArticles(this.state.search);
+    this.setState({ articles });
+  }
+
   render() {
+    //search funcionality here? Look closer at this
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <Search 
+          search={search}
+          onChange={this.handleChange}
+          onSearch={this.handleSearch}
+          onSubmit={this.handleSubmit}
+        />
+
+        <ArticleList articles={articles}/>
+      </>
+    );
   }
 }
